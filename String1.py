f5c0d34d97e0dc1844ce27204dc25aa751e0b65b
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2022-04-11 12:00:04 -0500

    Update String1.py

--- String1.py
+++ String1.py
@@ -7,3 +7,8 @@ print("hello\r67\r90\r")
 print("pinki \a pk")
 print(r"C:\new\tiket")
 print(dir(str))
+print(str(str1)+"Lai")
+print(ord("5"))
+print(chr(65))
+str1="lavanya"
+# str1[0]='K'//gives you an error
